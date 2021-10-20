76bb52365aaf995f6897b456353e30ab24ac1d21
Manish Kumar <manish.vickey93@gmail.com>
2021-10-20 22:49:31 +0530

    Unit test for producer for success scenario.

---
+++ library-events-producer/src/main/java/com/learnkafka/constant/LibraryEventConstants.java
@@ -0,0 +1,6 @@
+package com.learnkafka.constant;
+
+public class LibraryEventConstants {
+
+    public static final String TOPIC = "library-events";
+}
--- library-events-producer/src/test/java/com/learnkafka/unit/producer/LibraryEventsProducerUnitTest.java
+++ library-events-producer/src/test/java/com/learnkafka/unit/producer/LibraryEventsProducerUnitTest.java
@@ -2,10 +2,13 @@ package com.learnkafka.unit.producer;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.learnkafka.constant.LibraryEventConstants;
 import com.learnkafka.domain.Book;
 import com.learnkafka.domain.LibraryEvent;
 import com.learnkafka.producer.LibraryEventsProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.TopicPartition;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -13,6 +16,8 @@ import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.SettableListenableFuture;
 
 import java.util.concurrent.ExecutionException;
@@ -55,5 +60,43 @@ public class LibraryEventsProducerUnitTest {
         assertThrows(Exception.class, ()->eventProducer.sendLibraryEvent_Approach2(libraryEvent).get());
 
         //then
+    }
+
+    @Test
+    void sendLibraryEvent_Approach2_success() throws JsonProcessingException, ExecutionException, InterruptedException {
+        //given
+        Book book = Book.builder()
+                .bookId(123)
+                .bookAuthor("Manish")
+                .bookName("Kafka using Spring Boot")
+                .build();
+
+        LibraryEvent libraryEvent = LibraryEvent.builder()
+                .libraryEventId(null)
+                .book(book)
+                .build();
+
+        String record = objectMapper.writeValueAsString(libraryEvent);
+
+        SettableListenableFuture future = new SettableListenableFuture();
+
+        ProducerRecord<Integer, String> producerRecord = new ProducerRecord<>(LibraryEventConstants.TOPIC, libraryEvent.getLibraryEventId(), record);
+
+        RecordMetadata recordMetadata = new RecordMetadata(
+                new TopicPartition(LibraryEventConstants.TOPIC, 1),
+                1, 1, 342, System.currentTimeMillis(), 1, 2);
+
+        SendResult<Integer, String> sendResult = new SendResult<>(producerRecord, recordMetadata);
+
+        future.set(sendResult);
+
+        when(kafkaTemplate.send(isA(ProducerRecord.class))).thenReturn(future);
+
+        ListenableFuture<SendResult<Integer, String>> listenableFuture = eventProducer.sendLibraryEvent_Approach2(libraryEvent);
+
+        SendResult<Integer, String> sendResult1 = listenableFuture.get();
+
+        assert sendResult1.getRecordMetadata().partition() == 1;
+
     }
 }
